0c5f39a6deb3da5eaccf534a2329aca59ab259fe
vincenzo95 <vscotti95@gmail.com>
2018-09-27 18:26:32 +0200

    Complete script for silences extraction through praat and clean intensity tier

--- praat_scripts/post_processing/extra/silences_identification_through_intensity_outliers.py
+++ praat_scripts/post_processing/extra/silences_identification_through_intensity_outliers.py
@@ -14,6 +14,8 @@ SILENCES_FROM_INTENSITY = "_silences_from_intensity.csv"
 # Change in case OS is not MacOS
 PRAAT = "/Applications/Praat.app/Contents/MacOS/Praat"
 RUN_OPTIONS = "--run"
+# Add full path to praat script
+PRAAT_SCRIPT = "silences_info_extraction.praat"
 
 DTYPE = {"complete_event_file": str,
          "segmented_event_file": str,
@@ -45,58 +47,42 @@ def get_time_step(param_df_fn):
     return ts
 
 
-def extract_silences_as_intensity_outliers(silences_df_fn, window_size, intensity_tier_df_fn, n_sigma):
-    intensity_tier_df = pd.read_csv(intensity_tier_df_fn, dtype=DTYPE_INTENSITY_TIER)
-    intensity_values = intensity_tier_df["Intensity"]
-    mean = np.mean(intensity_values, axis=0)
-    sd = np.std(intensity_values, axis=0)
-    query = "Intensity < " + str(mean) + " - " + str(n_sigma * sd) + " or Intensity > " + str(mean) + " + " + str(n_sigma * sd)
-    intensity_tier_df = intensity_tier_df.query(query)
-    ts = intensity_tier_df["Time"].values
-    tmp_intervals = list(map(lambda x: list((x - (window_size / 2), x + (window_size + 2))), ts))
-    intervals = sorted(tmp_intervals, key=itemgetter(0))
-    consecutives = []
-    st = intervals[0][0]
-    for i in range(len(intervals)-1):
-        if intervals[i][1] != intervals[i+1][0]:
-            consecutives.append((st, intervals[i][1]))
-            st = intervals[i+1][0]
-    consecutives.append((st, intervals[-1][1]))
-    consecutives[0][0] = 0
-    #TODO check how to set end time efficiently
-    segments = [(x, y, "silence") for x, y in consecutives]
-    start = [x[1] for x in consecutives[:-1]]
-    end = [x[0] for x in consecutives[1:]]
-    segments += [(x, y, "speech") for x, y in list(zip(start, end))]
-    segments = sorted(segments, key=itemgetter(0))
-    silences_df = pd.DataFrame(data=segments, columns=["start_time", "end_time", "silence"])
-    silences_df.to_csv(silences_df_fn, index=False)
+def extract_silences_as_intensity_outliers(audio_fn, silences_df_fn, time_step, intensity_tier_df_fn, praat_parameters):
+    # TODO controllare che i nomi dei nuovi file vengano inseriti correttamente
+    subprocess.call([PRAAT, RUN_OPTIONS, PRAAT_SCRIPT, audio_fn,
+                     intensity_tier_df_fn,
+                     silences_df_fn,
+                     str(praat_parameters.silence_threshold_db),
+                     str(praat_parameters.minimum_silent_interval_duration),
+                     str(praat_parameters.minimum_sounding_interval_duration),
+                     str(time_step)])
+    return 1
 
 
-def silences_identification_through_intensity_serial(audio_info_df, audio_info_df_fn, audio_param_it_list, dest_path, new_column, n_sigma):
-    audio_sil_ws_it_list = list(map(lambda x: list(x[0], gen_file_name(x[0], dest_path), get_win_size(x[1]), x[2]), audio_param_it_list))
-    for tmp, sil_df_fn, win_size, it_df_fn in audio_sil_ws_it_list:
-        extract_silences_as_intensity_outliers(sil_df_fn, win_size, it_df_fn, n_sigma)
+def silences_identification_through_intensity_serial(audio_info_df, audio_info_df_fn, audio_param_it_list, praat_parameters, dest_path, new_column):
+    audio_sil_ws_it_list = list(map(lambda x: list(x[0], gen_file_name(x[0], dest_path), get_time_step(x[1]), x[2]), audio_param_it_list))
+    for audio_fn, sil_df_fn, win_size, it_df_fn in audio_sil_ws_it_list:
+        extract_silences_as_intensity_outliers(audio_fn, sil_df_fn, win_size, it_df_fn, praat_parameters)
     sil_df_fn_list = [(x, y) for x, y, z, t in audio_sil_ws_it_list]
     sil_df = pd.DataFrame(data=sil_df_fn_list, columns=new_column)
     audio_info_df = audio_info_df.join(sil_df)
     audio_info_df.to_csv(audio_info_df_fn, index=False)
 
 
-def silences_identification_through_intensity_parallel(audio_info_df, audio_info_df_fn, audio_param_it_list, dest_path, new_column, n_sigma, n):
-    audio_sil_ws_it_list = list(map(lambda x: list(x[0], gen_file_name(x[0], dest_path), get_win_size(x[1]), x[2]), audio_param_it_list))
-    Parallel(n_jobs=n, backend="threading")(delayed(extract_silences_as_intensity_outliers)(y, z, t, n_sigma) for x, y, z, t in audio_sil_ws_it_list)
+def silences_identification_through_intensity_parallel(audio_info_df, audio_info_df_fn, audio_param_it_list, praat_parameters, dest_path, new_column, n):
+    audio_sil_ws_it_list = list(map(lambda x: list(x[0], gen_file_name(x[0], dest_path), get_time_step(x[1]), x[2]), audio_param_it_list))
+    Parallel(n_jobs=n, backend="threading")(delayed(extract_silences_as_intensity_outliers)(x, y, z, t, praat_parameters) for x, y, z, t in audio_sil_ws_it_list)
     sil_df_fn_list = [(x, y) for x, y, z, t in audio_sil_ws_it_list]
     sil_df = pd.DataFrame(data=sil_df_fn_list, columns=new_column)
     audio_info_df = audio_info_df.join(sil_df)
     audio_info_df.to_csv(audio_info_df_fn, index=False)
 
 
-def identify_silences(audio_info_df_fn, dest_path, n_sigma=2, parallel=False, n_jobs=-1):
+def identify_silences(audio_info_df_fn, praat_parameters, dest_path, parallel=False, n_jobs=-1):
     df = pd.read_csv(audio_info_df_fn, dtype=DTYPE)
     audio_param_it_list = df[["complete_event_file", "parameters_file", "clean_intensity_tier_file"]].values
     new_column = ["complete_event_file", "silences_from_intensity_file"]
     if parallel:
-        silences_identification_through_intensity_parallel(df, audio_info_df_fn, audio_param_it_list, dest_path, new_column, n_sigma, n_jobs)
+        silences_identification_through_intensity_parallel(df, audio_info_df_fn, audio_param_it_list, praat_parameters, dest_path, new_column, n_jobs)
     else:
-        silences_identification_through_intensity_serial(df, audio_info_df_fn, audio_param_it_list, dest_path, new_column, n_sigma)
+        silences_identification_through_intensity_serial(df, audio_info_df_fn, audio_param_it_list, praat_parameters, dest_path, new_column)
